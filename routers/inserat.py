35c8c7bbdf7e36f909c389893fbf9297e9decb51
AbdallahZerfaoui <zerfaouiabdallah@gmail.com>
2025-11-21 21:32:52 +0100

    refactor(inserat.py, inserate.py): improve code readability and structure by formatting function definitions and enhancing docstrings

--- routers/inserat.py
+++ routers/inserat.py
@@ -1,17 +1,22 @@
 from scrapers.inserat import get_inserate_details
 from fastapi import APIRouter, HTTPException
 from utils.browser import PlaywrightManager
+from config import INSERAT_URL_TEMPLATE
 
 router = APIRouter()
 
+
 @router.get("/inserat/{id}")
-async def get_inserat(id: str):
+async def get_inserat(id: str) -> dict:
+    """
+    Fetch details of a specific inserat by ID.
+    """
     browser_manager = PlaywrightManager()
     await browser_manager.start()
     try:
         page = await browser_manager.new_context_page()
-        url = f"https://www.kleinanzeigen.de/s-anzeige/{id}"
+        url = INSERAT_URL_TEMPLATE.format(id=id)
         result = await get_inserate_details(url, page)
         return {"success": True, "data": result}
     finally:
-        await browser_manager.close() 
+        await browser_manager.close()
